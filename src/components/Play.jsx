d35574f331adc9a76e3bcc6486359e990a923e89
tomohiro <tomohirofarm@gmail.com>
2023-01-27 11:01:45 +0900

    douga

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import videoBg from "../assets/video_17248673274339-jVIB0zK6.MP4";
+import "./Play.css"
 
 const Play = () => {
   return (
-    <div>ここに動画を載せたいよ</div>
+    <div className='main'>
+      <div className='overlay'></div>
+     <video src={videoBg} autoPlay loop muted />
+      <div className='content'>
+       <h1>Welcome!</h1>
+       <p>To My Room</p>
+      </div>
+    </div>
   )
 }
 
